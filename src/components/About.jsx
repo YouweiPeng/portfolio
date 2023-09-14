6d48a1a80913c6963c4599b2dad88e4e44d244ca
YouweiPeng <98921666+YouweiPeng@users.noreply.github.com>
2023-09-14 10:09:53 -0600

    Modify content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,10 +47,9 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
+        I'm a skilled software engineer with full stack development experience in programming languages like Python, Java, C/C++, SQL, JavaScript,  HTML, and CSS 
+        and expertise in frontend frameworks like React, Vue, React Native and backend frameworks like Django, Spring Boot, Node.js, SQL and NoSQL databases.
+        I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
